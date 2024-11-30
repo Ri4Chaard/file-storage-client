1e49280d860af5c5dfae4147911b88315bbd7460
Ri4Chaard <polovnikovyaroslav@gmail.com>
2024-11-30 18:41:59 +0300

    File preview updated

--- components/shared/file-preview-modal.tsx
+++ components/shared/file-preview-modal.tsx
@@ -27,25 +27,59 @@ export const FilePreviewModal: React.FC<Props> = ({
         onClose();
     };
 
-    const isVideoFile = (fileName: string) => {
-        const videoExtensions = [".mp4", ".avi", ".mov", ".mkv"];
-        return videoExtensions.some((ext) =>
-            fileName.toLowerCase().endsWith(ext)
-        );
+    const getFileType = (fileName: string) => {
+        const extension = fileName.split(".").pop()?.toLowerCase();
+        if (!extension) return "unknown";
+
+        const videoExtensions = ["mp4", "avi", "mov", "mkv"];
+        const imageExtensions = ["jpg", "jpeg", "png", "gif", "bmp", "webp"];
+
+        if (videoExtensions.includes(extension)) return "video";
+        if (imageExtensions.includes(extension)) return "image";
+
+        return "unknown";
+    };
+
+    const renderPreview = () => {
+        const fileType = getFileType(fileName);
+
+        switch (fileType) {
+            case "video":
+                return (
+                    <video
+                        controls
+                        src={previewUrl}
+                        className="w-full h-auto"
+                    />
+                );
+            case "image":
+                return (
+                    <img
+                        src={previewUrl}
+                        alt={fileName}
+                        className="w-full h-auto pointer-events-none"
+                    />
+                );
+
+            default:
+                return (
+                    <p className="text-center">
+                        Перегляд недоступний для такого типу файлу.
+                    </p>
+                );
+        }
     };
 
     return (
         <Dialog open={open} onOpenChange={handleClose}>
-            <DialogContent className={cn("sm:max-w-[425px]", className)}>
+            <DialogContent className={cn("sm:max-w-[600px]", className)}>
                 <DialogHeader>
-                    <DialogTitle>{fileName}</DialogTitle>
+                    <DialogTitle>{fileName.substring(14)}</DialogTitle>
                 </DialogHeader>
-                {isVideoFile(fileName) ? (
-                    <video controls src={previewUrl} />
-                ) : (
-                    <img src={previewUrl} alt={fileName} />
-                )}
-                <DialogFooter></DialogFooter>
+                <div className="flex items-center justify-center p-4">
+                    {renderPreview()}
+                </div>
+                <DialogFooter />
             </DialogContent>
         </Dialog>
     );
